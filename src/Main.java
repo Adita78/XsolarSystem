8902308f863351932aa5cf19c6a1057eef32d67b
Adita78 <captari.ad@gmail.com>
2022-12-18 12:48:33 -0500

    I have fixed issue in the branch is ready to merge with TOM

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
                XX.sun=2;
         FeatureSun Xeron = new FeatureSun();
         Xeron.Heat="12324234";
-        Xeron.size=1221334234;
-        Xeron.Temperature="3434";
+        //Xeron.size=1221334234;
+        Xeron.Temperature=700;
     }
 }
